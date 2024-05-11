3dda4630207181fd6bdeb2ff131cb97fe42f660f
Shubham Singhal <shubhamsinghal@gofynd.com>
2024-05-11 16:44:36 +0530

    Hi World App Created

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,41 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <HelloWorld :msg="message" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import axios from 'axios'
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      message: '',
+    }
+  },
+  mounted() {
+    this.getAppMessage()
+    document.title = 'Hello World App'
+  },
+  methods: {
+    async getAppMessage() {
+      await axios
+        .get('/index/')
+        .then(response => {
+          this.message = response.data.message
+        })
+        .catch(error => {
+          console.log(error.response)
+        })
+
+    }
   }
 }
 </script>
